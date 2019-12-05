a3173d1f44119711df0f72ce42bda8b26dc7e64b
Akin <alexandre@fluxonaut.com>
2019-12-05 04:05:42 -0300

    day 1 part two

--- 2019/solutions/day_1.py
+++ 2019/solutions/day_1.py
@@ -1,16 +1,29 @@
 import math
 from utils import read_input, write_output
-from functools import reduce
+from functools import reduce, partial
+from typing import Callable, Iterable
 
 DAY = 1
 
 def calculate_fuel(mass: int) -> int:
     return (math.floor(mass/3) - 2)
 
-def reducer(accumulator: int, element: str) -> int:
-    return (accumulator + calculate_fuel(int(element)))
+def reducer(func: Callable[[int], int], accumulator: int, element: str) -> int:
+    return (accumulator + func(int(element)))
+
+def part_one(masses: Iterable) -> int:
+    partial_reducer = partial(reducer, calculate_fuel)
+    return reduce(partial_reducer, masses, 0)
+
+def part_two(masses: Iterable) -> int:
+    partial_reducer = partial(reducer, calculate_fuel_fuel)
+    return reduce(partial_reducer, masses, 0)
+
+def calculate_fuel_fuel(mass: int) -> int:
+    result = (math.floor(mass/3) - 2)
+    return result + calculate_fuel_fuel(result) if result > 0 else 0
 
 if __name__ == "__main__":
     masses = read_input(DAY)
-    total = reduce(reducer, masses, 0)
+    total = part_two(masses)
     write_output(DAY, total)
--- 2019/tests/test_day_1.py
+++ 2019/tests/test_day_1.py
@@ -1,5 +1,5 @@
 import pytest
-from solutions.day_1 import calculate_fuel
+from solutions.day_1 import calculate_fuel, calculate_fuel_fuel
 
 @pytest.mark.parametrize(
     "input, expected_result",
@@ -10,5 +10,15 @@ from solutions.day_1 import calculate_fuel
         (100756, 33583),
     ]
 )
-def test(input, expected_result):
+def test_one(input, expected_result):
     assert calculate_fuel(input) == expected_result
+
+@pytest.mark.parametrize(
+    "input, expected_result",
+    [
+        (1969, 966),
+        (100756, 50346)
+    ]
+)
+def test_two(input, expected_result):
+    assert calculate_fuel_fuel(input) == expected_result
